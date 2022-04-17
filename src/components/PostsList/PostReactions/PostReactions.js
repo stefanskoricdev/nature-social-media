719994bfdf2ce94c9e6ff8e574b9f9739beec79b
Stefan Skoric <stephanskoric@gmail.com>
2022-04-17 17:58:57 +0200

    Add upvote and downvote functionality to Post component

--- src/components/PostsList/PostReactions/PostReactions.js
+++ src/components/PostsList/PostReactions/PostReactions.js
@@ -1,26 +1,139 @@
 import styles from "./PostReactions.module.scss";
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
 import { AiFillLike, AiFillDislike } from "react-icons/ai";
 import { FaCommentAlt } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import { useHttp } from "../../../hooks/useHttp";
+import { POSTS_URL } from "../../../util/constants";
+import AppContext from "../../../store/AppProvider";
+import AuthContext from "../../../store/AuthProvider";
+import Backdrop from "../../UI/Backdrop/Backdrop";
+import Modal from "../../UI/Modal/Modal";
+import ErrorModal from "../../UI/Modal/ErrorModal/ErrorModal";
 
 const PostReactions = ({ post }) => {
-  const didUserLike = post.upVotes.find((user) => user === "stefan.skoric");
-  const didUserDislike = post.downVotes.find(
-    (user) => user === "stefan.skoric"
+  const { sendRequest: sendVoteRequest, error, setError } = useHttp();
+
+  const appCtx = useContext(AppContext);
+  const { posts, setPosts } = appCtx;
+
+  const authCtx = useContext(AuthContext);
+  const { currentUser } = authCtx;
+
+  const didUserLike = post.upVotes.find(
+    (user) => user === currentUser.username
   );
+  const didUserDislike = post.downVotes.find(
+    (user) => user === currentUser.username
+  );
+
+  const targetedPostIndex = posts.findIndex(
+    (postItem) => postItem.id === post.id
+  );
+  const targetedPost = posts[targetedPostIndex];
+
+  const handleNeutralizeLike = () => {
+    let updatedItem;
+    const filteredVotes = targetedPost.upVotes.filter(
+      (vote) => vote !== currentUser.username
+    );
+    updatedItem = { ...targetedPost, upVotes: [...filteredVotes] };
+    setPosts((prevState) => {
+      prevState[targetedPostIndex] = updatedItem;
+      return [...prevState];
+    });
+  };
+
+  const handleNeutralizeDislike = () => {
+    let updatedItem;
+    const filteredVotes = targetedPost.downVotes.filter(
+      (vote) => vote !== currentUser.username
+    );
+    updatedItem = { ...targetedPost, downVotes: [...filteredVotes] };
+    setPosts((prevState) => {
+      prevState[targetedPostIndex] = updatedItem;
+      return [...prevState];
+    });
+  };
+
+  const updateUi = (additionalData) => {
+    setPosts((prevState) => {
+      const { event, updatedItem } = additionalData;
+      if (event.target.id === "upvote") {
+        if (didUserLike) {
+          handleNeutralizeLike();
+        }
+      } else {
+        if (didUserDislike) {
+          handleNeutralizeDislike();
+        }
+      }
+      prevState[targetedPostIndex] = updatedItem;
+      return [...prevState];
+    });
+  };
+
+  const voteRequestHandler = (event) => {
+    let updatedItem;
+    if (event.target.id === "upvote") {
+      updatedItem = {
+        ...targetedPost,
+        upVotes: [...targetedPost.upVotes, currentUser.username],
+        downVotes: targetedPost.downVotes.filter(
+          (vote) => vote !== currentUser.username
+        ),
+      };
+    } else {
+      updatedItem = {
+        ...targetedPost,
+        downVotes: [...targetedPost.downVotes, currentUser.username],
+        upVotes: targetedPost.upVotes.filter(
+          (vote) => vote !== currentUser.username
+        ),
+      };
+    }
+    sendVoteRequest(
+      {
+        url: `${POSTS_URL}/${post.id}`,
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: { ...updatedItem },
+      },
+      updateUi,
+      { event, updatedItem }
+    );
+  };
+
+  const handleBackdropClose = () => {
+    setError(null);
+  };
 
   return (
     <Fragment>
+      {error && (
+        <Backdrop handleClick={handleBackdropClose}>
+          <Modal>
+            <ErrorModal message={error} />
+          </Modal>
+        </Backdrop>
+      )}
       <section className={styles.PostReactions}>
-        <button className={styles.ReactionItemBtn}>
+        <button
+          id="upvote"
+          onClick={voteRequestHandler}
+          className={styles.ReactionItemBtn}
+        >
           <AiFillLike
             fontSize="2.2rem"
             color={didUserLike ? "rgb(0, 185, 96)" : "rgb(168, 167, 167)"}
           />
           <span>{post.upVotes.length}</span>
         </button>
-        <button className={styles.ReactionItemBtn}>
+        <button
+          id="downvote"
+          onClick={voteRequestHandler}
+          className={styles.ReactionItemBtn}
+        >
           <AiFillDislike
             color={didUserDislike ? "rgb(0, 185, 96)" : "rgb(168, 167, 167)"}
             fontSize="2.2rem"
--- src/hooks/useHttp.js
+++ src/hooks/useHttp.js
@@ -3,7 +3,7 @@ import { useState } from "react";
 export const useHttp = () => {
   const [isLoading, setIsLoading] = useState(null);
   const [error, setError] = useState(null);
-  const sendRequest = (requestConfig, applyData) => {
+  const sendRequest = (requestConfig, applyData, additionalData) => {
     const { url, method, headers, body } = requestConfig;
     setIsLoading(true);
     fetch(url, {
@@ -15,14 +15,22 @@ export const useHttp = () => {
         if (res.ok) {
           return res.json();
         } else {
-          console.log("Here");
           throw new Error(
             "Ooops something went wrong. Please try again later."
           );
         }
       })
       .then((data) => {
-        setIsLoading(false);
+        //Avoid awkward glitches when request is fast
+        setTimeout(() => {
+          setIsLoading(false);
+        }, [200]);
+        //Use additional data to update state if needed
+        if (additionalData) {
+          applyData(additionalData);
+          return;
+        }
+        //if not, use response from server to update state
         applyData(data);
       })
       .catch((err) => {
